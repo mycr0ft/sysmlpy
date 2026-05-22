64f484150b4f040faab1466d77fe01d3168c15f6
Jon R. Fox <drjonfox@gmail.com>
2026-05-22 12:18:19 -0500

    Bump version to 0.19.1

--- src/sysmlpy/__init__.py
+++ src/sysmlpy/__init__.py
@@ -15,7 +15,7 @@ __all__ = [
     "analyze", "SemanticIssue", "SemanticAnalyzer",
 ]
 __author__ = "Jon Fox"
-__version__ = "0.15.0"
+__version__ = "0.19.1"
 
 from sysmlpy.usage import (
     Item, Attribute, Part, Port, Action, Reference, UseCase, Requirement, Interface, Message,
